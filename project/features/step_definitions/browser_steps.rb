b3646796d647c72375027e9d96ad20561b2f8e07
KateLugova <katya.lugovaya@gmail.com>
2012-10-10 09:47:15 +0100

    add step to check the link on a page

--- project/features/step_definitions/browser_steps.rb
+++ project/features/step_definitions/browser_steps.rb
@@ -2,3 +2,9 @@ Given /^opened "(.*?)" page$/ do |url|
  visit url
   sleep (5)
 end
+
+When /^I should see "(.*?)" link$/ do |link|
+    page.should have_link(link)
+end
+
+
--- project/features/test.feature
+++ project/features/test.feature
@@ -4,6 +4,8 @@ Feature: Testing open web page
   Scenario: Opening page
 
     Given opened "http://sky.com" page
+    When I should see "My Sky" link
+
 
 
 
