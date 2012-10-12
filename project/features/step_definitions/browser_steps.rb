5805c611accf4144c1a8d4dcdb4319e48fe9f5ac
Denys Tatarchenko <aegis457@gmail.com>
2012-10-12 13:53:41 +0100

    Removed duplicated steps

--- project/features/step_definitions/browser_steps.rb
+++ project/features/step_definitions/browser_steps.rb
@@ -3,11 +3,6 @@ Given /^opened "(.*?)" page$/ do |url|
  visit url
   end
 
-Then /^I should see "(.*?)" link$/ do |link|
-    page.should have_link(link)
-end
-
-
 When /^I click "(.*)" link$/ do |link|
   click_link link
 end
@@ -47,11 +42,6 @@ end
 
 Then /^I should see "(.*?)" page$/ do |page|
   page.should have_content(page)
-end
-
-
-Then /^I should see image "(.*?)" title on page$/ do |name|
-  page.should have_selector(:xpath, "//img[contains(@title,'#{name}')]")
 end
 
 Then /^I should see "(.*?)" on the page$/ do |name|
