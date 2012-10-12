e9bb983254600ee5a1c0c7f8ffc25293cf9398f7
Denys Tatarchenko <aegis457@gmail.com>
2012-10-12 10:52:14 +0100

    Series Selection Row check

---
+++ project/features/series_selection_row.feature
@@ -0,0 +1,16 @@
+
+Feature: Testing Series Selection Row
+
+  Scenario: Opening page, and trying to find the elements
+
+    Given opened "http://go.test.sp.bskyb.com/vod/content/SKYKIDS/Browse_by_Genre/RegressionPack_Series_Selection_Row_%28DO_NOT_TOUCH!%29/content/default/promoPage.do" page
+    Then I should see "Series Selection Row" on the page
+    Then I should see 7 "Series" on the page
+    Then I want to get all content names for "Series Selection Row"
+    Then I should see "Sel Cartoon Network Series1" title present in row
+    Then I should see image of "Sel Cartoon Network Series1" title
+    When I click "Jungle Bunnies" link
+    Then I should see "Jungle Bunnies" page
+
+
+
--- project/features/step_definitions/browser_steps.rb
+++ project/features/step_definitions/browser_steps.rb
@@ -1,3 +1,6 @@
+require_relative  '../../lib/SeriesSelectionRow'
+
+
 Given /^opened "(.*?)" page$/ do |url|
  visit url
   sleep (5)
@@ -20,3 +23,34 @@ Then /^I should see image "(.*?)" title on page$/ do |name|
   page.should have_selector(:xpath, "//img[contains(@title,'#{name}')]")
 end
 
+Then /^I should see "(.*?)" on the page$/ do |name|
+  case name
+    when 'Series Selection Row'
+      @series=SeriesSelectionRow.new
+    page.should have_selector(:xpath, @series.xpath_of_elements(1))
+  end
+
+end
+
+Then /^I should see (\d+) "(.*?)" on the page$/ do |number, name|
+  case name
+    when 'Series'
+      page.all(:xpath, @series.counting_xpath).length.should == number.to_i
+  end
+end
+
+Then /^I should see "(.*?)" title present in row$/ do |title|
+  page.should have_selector(:xpath, @series.get_name_of_elements(title))
+end
+
+Then /^I should see image of "(.*?)" title$/ do |title|
+  page.should have_selector(:xpath, @series.get_presence_of_image(title))
+end
+
+
+Then /^I want to get all content names for "(.*?)"$/ do |row_type|
+  case row_type
+    when "Series Selection Row"
+      all(:xpath, @series.all_names).map{|element| p element[:title]}
+  end
+end
