3d249711a015820a6096b5e9c0d97c00b8d213f0
Jenyaya <jenyaya@online.ua>
2012-10-31 12:57:10 +0000

    removed comments

--- project/features/lib/DynamicRow.rb
+++ project/features/lib/DynamicRow.rb
@@ -8,11 +8,4 @@ class DynamicRow < AllRows
     @movie_name_xpath = "#{@main_xpath}//h3"
   end
 
-  #def is_image_present?(name)
-  #  image_xpath = "#{@movie_name_xpath}[descendant-or-self::*[contains(@title,'#{name}')]]/preceding-sibling::*[descendant-or-self::img]"
-  #  # image_xpath = "#{@movie_name_xpath}[contains(@title,'#{name}')]/preceding-sibling::*[descendant-or-self::img]"
-  #  return Capybara.page.should have_selector(:xpath, image_xpath)
-  #end
-
-
 end
